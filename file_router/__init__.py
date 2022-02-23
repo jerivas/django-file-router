d5edf7a3611a9a06fad91e0650fa30ed976ef79a
Ed Rivas <ed@jerivas.com>
2022-02-22 22:42:24 -0600

    feat: allow views to manually specify their url pattern and name

--- file_router/__init__.py
+++ file_router/__init__.py
@@ -2,7 +2,6 @@ import os
 import re
 from importlib import import_module
 
-from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpResponse
 from django.template import RequestContext, Template
 from django.urls import path
@@ -12,9 +11,10 @@ __version__ = "0.1.0"
 DISALLOWED_CHARS = re.compile(
     "|".join(
         [
-            r"^-",  # Dash at the start
+            r"^-+",  # Leading dashes
             r"[<>]",  # Angle brackets (url param wrapper)
             r"\w+\:",  # Letters followed by colon (path converters)
+            r"-+$",  # Trailing dashes
         ]
     )
 )
@@ -35,20 +35,25 @@ def file_patterns(start_dir, append_slash=False):
         for file in files:
             if not file.endswith(".py"):
                 continue
+
             module_path = f"{root}/{file}".replace(".py", "").replace("/", ".")
-            try:
-                module = import_module(module_path)
-            except ImportError as exc:
-                raise ImproperlyConfigured(
-                    f"Failed to import {module_path}. Ensure it's a valid Python file."
-                ) from exc
+            module = import_module(module_path)
             view_fn = getattr(module, "view", None)
             if not callable(view_fn):
                 continue
-            url = "" if file == "index.py" else file.replace(".py", "")
-            url = start_dir_re.sub("", f"{root}/{url}").strip("/")
-            url = (url + "/") if append_slash else url
-            urlname = DISALLOWED_CHARS.sub("", TO_DASHES.sub("-", url))
+
+            try:
+                url = view_fn.url
+            except AttributeError:
+                url = "" if file == "index.py" else file.replace(".py", "")
+                url = start_dir_re.sub("", f"{root}/{url}").strip("/")
+                url = (url + "/") if append_slash else url
+
+            try:
+                urlname = view_fn.urlname
+            except AttributeError:
+                urlname = DISALLOWED_CHARS.sub("", TO_DASHES.sub("-", url))
+
             patterns.append(path(url, view_fn, name=urlname))
     return patterns
 
