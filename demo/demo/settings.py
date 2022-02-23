9c6027a3395ae92e1c2fcd5dec3e87e7cd626af8
Ed Rivas <ed@jerivas.com>
2022-02-23 00:27:25 -0600

    ci: fix demo project DB settings

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = "demo.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"),
     }
 }
 
